ced1683ecd74d0c2dc0d0bd2fe9ef1c6828ea64d
GilbertSun <szb4321@gmail.com>
2016-07-26 13:41:27 +0800

    refine template to Hybrid App

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-new Vue({
-  el: 'body',
-  components: { App }
-});
+let Init = () => {
+  new Vue({
+    el: 'body',
+    components: { App }
+  });
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // For the Hybrid App
+  Hybrid.Init(() => {
+    global.HOST = site_url; // only can get this when Hybrid.Init is called
+    Init();
+  });
+} else {
+  global.HOST = 'http://amptest.wisedu.com'
+  Init();
+}
+
